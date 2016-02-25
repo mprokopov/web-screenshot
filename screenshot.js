bba88c51db3ae6b5a588adaf376f1839a35b0069
rootzoll <christian@geektank.de>
2016-02-25 22:32:47 +0100

    updated libs, stateless, base64

--- screenshot.js
+++ screenshot.js
@@ -4,8 +4,11 @@ var webshot = require('webshot'),
     crypto = require('crypto'),
     fs = require('fs'),
     lwip = require('lwip');
+    stream = require('stream');
+    base64 = require('base64-stream');
 
 app.get('/', function (req, res) {
+
     var url = req.query.url,
         scale = req.query.scale ? req.query.scale : '1',
         urlHash = crypto.createHash('md5').update(url + '-scale:1').digest('hex'),
@@ -23,22 +26,25 @@ app.get('/', function (req, res) {
 
     console.log(url, urlHash, imgPath);
 
-    if (fs.existsSync(imgPath)) {
-        res.setHeader('content-type', 'image/png');
-        fs.createReadStream(imgPath).pipe(res);
-    } else {
         webshot(url, imgOriginalPath, options, function (err) {
             if (err) {
+
+                /*
+                 * FAIL
+                 */
+
                 res.status(500);
                 res.send('An error occured: ' + err);
                 console.error(err);
-            } else {
-                res.setHeader('content-type', 'image/png');
 
-                if (fs.existsSync(imgPath)) {
-                    fs.createReadStream(imgPath).pipe(res);
-                } else {
-                    lwip.open(imgOriginalPath, function (err, image) {
+            } else {
+
+                /*
+                 * WIN
+                 */
+                res.setHeader('Content-Type', 'image/png');
+                lwip.open(imgOriginalPath, function (err, image) {
+                    try {
                         image.batch()
                             .scale(parseFloat(scale))
                             .writeFile(imgPath, function (err) {
@@ -48,13 +54,45 @@ app.get('/', function (req, res) {
                                     console.error(err);
                                     return;
                                 }
-                                fs.createReadStream(imgPath).pipe(res);
+                                var stream = fs.createReadStream(imgPath)
+                                .on('error', function (err) {
+                                        res.status(500);
+                                        res.send('An error occured: ' + err);
+                                        console.error(err);
+                                        return;
+                                })
+                                .on('close', function () {
+                                    console.log("OK & delete");
+                                    // delete after writing to stream
+                                    fs.unlink(imgPath);
+                                });
+
+                                // add base64 decoding if wanted by url parameter
+                                if (req.query.base64) {
+                                    console.log("Content-Transfer-Encoding > BASE64");
+                                    res.setHeader('Content-Transfer-Encoding', 'base64');
+                                    //res.send('data:image/png;base64,');
+                                    stream.pipe(base64.encode())
+                                    .pipe(res);
+                                } else {
+                                    stream.pipe(res);
+                                }
                             });
-                    });
+
+                    } catch (e) {
+                        console.log("FAIL - try to clean up:"+e);
+                        console.dir(e);
+                        // delete after writing to stream
+                        fs.unlink(imgPath);
+                    }
+                    }
+
+                    );
                 }
-            }
+
         });
-    }
+
 });
 
 app.listen(2341);
+console.log('Running on port 2341 - for testing call: http://localhost:2341/?url=http://google.com');
