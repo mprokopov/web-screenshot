5fe589882947389a373dbb25674ac292f6bee0bf
rotzoll <christian@geektank.de>
2017-03-30 22:14:33 +0200

    oer hackathon - make more stable

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
 web-screenshot
 ==============
 
+## RUN AS DOCKER CONTAINER (for production)
+
+docker build -t="web-screenshot" .
+docker run -d --name web-screenshot -p 2341:2341 web-screenshot
+docker logs web-screenshot -f
+
+## RUN LOCAL (for dev)
+
+Make sure NodeJS is installed - tested with v6.10.1. (`nvm use`)
+
+INSTALL PHANTOM JS (global)
+
 To enable Woff support, follow [these steps](http://squallssck.github.io/blog/2013/03/07/about-how-to-make-phantomjs-support-google-web-fonts/).
 
-If you don't need Woff support, just run: `npm install phantomjs -g`.
+If you don't need Woff support, just run: `npm install -g phantomjs-prebuilt`.
+
+RUN APP
 
 To get the app going, do:
 
--- screenshot.js
+++ screenshot.js
@@ -7,7 +7,39 @@ var webshot = require('webshot'),
     stream = require('stream');
     base64 = require('base64-stream');
 
-app.get('/', function (req, res) {
+var runningProcessCounter = 0;
+
+var processRequest = function (req, res, loopcount) {
+
+    console.log("**************************");
+    var reqUrl = req.protocol + '://' + req.get('Host') + req.url;
+    console.log(reqUrl);
+
+    if (typeof loopcount == "undefined") loopcount = 0;
+
+    try {
+        if (runningProcessCounter>0) {
+            // drop request if over 500 requests waiting
+            if (runningProcessCounter>500) {
+                res.status(500);
+                res.send('service overload');
+                return;
+            }
+            console.log("BUSY REDIRECT");
+            setTimeout(function(){
+                res.status(302);
+                res.set('Location', reqUrl);
+                res.send(reqUrl);
+            }, 2000);
+            return;
+        }
+    } catch (e) {
+        console.error("EXCEPTION ON WAIT LOOP");
+    }
+
+    runningProcessCounter++;
+
+    try {
 
     var url = req.query.url,
         scale = req.query.scale ? req.query.scale : '1',
@@ -24,7 +56,8 @@ app.get('/', function (req, res) {
             renderDelay: 2000
         };
 
-    console.log(url, urlHash, imgPath);
+        console.log("PROCESSING START");
+        console.log(url, urlHash, imgPath);
 
         webshot(url, imgOriginalPath, options, function (err) {
             if (err) {
@@ -52,6 +85,7 @@ app.get('/', function (req, res) {
                                     res.status(500);
                                     res.send('An error occured: ' + err);
                                     console.error(err);
+                                    runningProcessCounter--;
                                     return;
                                 }
                                 var stream = fs.createReadStream(imgPath)
@@ -59,10 +93,12 @@ app.get('/', function (req, res) {
                                         res.status(500);
                                         res.send('An error occured: ' + err);
                                         console.error(err);
+                                        runningProcessCounter--;
                                         return;
                                 })
                                 .on('close', function () {
-                                    console.log("OK & delete");
+                                    console.log("PROCESSING DONE");
+                                    console.log(url, urlHash, imgPath);
                                     // delete after writing to stream
                                     fs.unlink(imgPath);
                                 });
@@ -81,6 +117,7 @@ app.get('/', function (req, res) {
 
                     } catch (e) {
                         console.log("FAIL - try to clean up:"+e);
+                        console.log(url, urlHash, imgPath);
                         console.dir(e);
                         // delete after writing to stream
                         fs.unlink(imgPath);
@@ -89,10 +126,22 @@ app.get('/', function (req, res) {
 
                     );
                 }
+                runningProcessCounter--;
 
         });
 
-});
+    } catch (e) {
+        res.status(500);
+        console.error("EXCEPTION");
+        console.error(url, urlHash, imgPath);
+        res.send('An error occured: ' + JSON.stringify(e));
+        console.error(e);
+        runningProcessCounter++;
+        return;  
+    }
 
+};
+
+app.get('/', processRequest);
 app.listen(2341);
 console.log('Running on port 2341 - for testing call: http://localhost:2341/?url=http://google.com');
